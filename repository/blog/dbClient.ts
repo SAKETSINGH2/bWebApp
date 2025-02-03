ca13eccd9ae4006ea0a2601254df73da189964ec
SAKETSINGH2 <saketsingh186@gmail.com>
2025-02-03 15:20:37 +0530

    changes

--- repository/blog/dbClient.ts
+++ repository/blog/dbClient.ts
@@ -6,6 +6,10 @@ const blogModelSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        description: {
+            type: String,
+            required: true,
+        },
         author: {
             type: String,
             required: true,
